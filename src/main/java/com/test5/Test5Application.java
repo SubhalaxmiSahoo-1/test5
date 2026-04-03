498f1b14e0b228082d7c6f210617e0704a7caaa5
Subhalaxmi <subjalaxmi2003sahoo@gmail.com>
2026-04-03 11:07:57 +0530

    Second Commit

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+
+        byte a = 10;
+        short b = 12;
+        int c = 11;
+        long d = 14;
+        float e = 15.0F;
+        char f = 'G';
+        String name = "Subha";
+
 		SpringApplication.run(Test5Application.class, args);
 	}
 
